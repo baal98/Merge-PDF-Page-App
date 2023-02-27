27b1b06b55d60226903896a876d7a56264115f88
BAAL98 <baal98@gmail.com>
2023-02-27 10:46:41 +0200

    initial create

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿using System.Drawing;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button1.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.sunshine1_copy4;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Lucida Sans", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.OldLace;
+            this.button1.Location = new System.Drawing.Point(78, 451);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(406, 142);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "merge PDFs";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button2.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.sunshine1_copy4;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Lucida Sans", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.OldLace;
+            this.button2.Location = new System.Drawing.Point(551, 451);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(406, 142);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "open PDF";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans Unicode", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(121, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(794, 81);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Моля ,изберете опция";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.sunshine;
+            this.ClientSize = new System.Drawing.Size(1110, 684);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,108 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "PDF files (*.pdf)|*.pdf";
+            openFileDialog.Multiselect = true;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                List<string> selectedFiles = new List<string>(openFileDialog.FileNames);
+                MergePDFs(selectedFiles);
+            }
+        }
+
+        private void MergePDFs(List<string> fileNames)
+        {
+            Document document = new Document();
+            PdfCopy pdfCopy = new PdfCopy(document, new FileStream("MergedPDF.pdf", FileMode.Create));
+            document.Open();
+            foreach (string fileName in fileNames)
+            {
+                PdfReader pdfReader = new PdfReader(fileName);
+                for (int i = 1; i <= pdfReader.NumberOfPages; i++)
+                {
+                    PdfImportedPage page = pdfCopy.GetImportedPage(pdfReader, i);
+                    pdfCopy.AddPage(page);
+                }
+                pdfReader.Close();
+            }
+            document.Close();
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PDF files (*.pdf)|*.pdf";
+            saveFileDialog.FileName = "MergedPDF.pdf";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.File.Copy("MergedPDF.pdf", saveFileDialog.FileName, true);
+                System.IO.File.Delete("MergedPDF.pdf");
+            }
+        }
+        
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            // Create an instance of the OpenFileDialog dialog
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // Set the file filter to allow only PDF files
+            openFileDialog.Filter = "PDF files (*.pdf)|*.pdf";
+
+            // Show the dialog and get the result
+            DialogResult result = openFileDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                // Get the path of the selected file
+                string filePath = openFileDialog.FileName;
+
+                // Create a ProcessStartInfo object to specify the process to start
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+
+                // Set the file name and arguments to open the selected file with the default program
+                startInfo.FileName = filePath;
+                startInfo.UseShellExecute = true;
+
+                // Start the process
+                Process.Start(startInfo);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Love Makes You Happy!";
+            label1.ForeColor = Color.Red;
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            label1.Update();
+            Timer timer = new Timer();
+            timer.Interval = 10000;
+            timer.Tick += (s, b) =>
+            {
+                label1.BackColor = Color.Transparent;
+                label1.Text = "Избери PDF за сливане";
+                label1.ForeColor = Color.Black;
+                label1.Size = new Size(200, 20);
+                label1.TextAlign = ContentAlignment.MiddleLeft;
+                timer.Stop();
+            };
+            timer.Start();
+        }
+
+    }
+}
